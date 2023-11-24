603304a3a223879ae3035fb39471b17300595e59
Kazae67 <Kazae.zaeka@gmail.com>
2023-11-24 12:49:28 +0100

    amélioration sécurité register & change password

--- src/Form/ChangePasswordFormType.php
+++ src/Form/ChangePasswordFormType.php
@@ -5,9 +5,13 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Regex;
 use App\DTO\ChangePasswordModel;
 
 class ChangePasswordFormType extends AbstractType
@@ -17,12 +21,45 @@ class ChangePasswordFormType extends AbstractType
         $builder
             ->add('oldPassword', PasswordType::class, [
                 'label' => 'Current Password',
-                'constraints' => new UserPassword(),
+                'constraints' => new UserPassword([
+                    'message' => 'The current password is incorrect.',
+                ]),
                 'attr' => ['autocomplete' => 'current-password'],
             ])
-            ->add('newPassword', PasswordType::class, [
-                'label' => 'New Password',
-                'attr' => ['autocomplete' => 'new-password'],
+            ->add('newPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The two password fields must match exactly.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options' => ['label' => 'New Password'],
+                'second_options' => ['label' => 'Repeat New Password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a new password',
+                    ]),
+                    new Length([
+                        'min' => 12,
+                        'max' => 64,
+                        'minMessage' => 'Your new password must be at least {{ limit }} characters long',
+                        'maxMessage' => 'Your new password must not be longer than {{ limit }} characters',
+                    ]),
+                    new Regex([
+                        'pattern' => '/[A-Z]/',
+                        'message' => 'Your new password must contain at least one uppercase letter',
+                    ]),
+                    new Regex([
+                        'pattern' => '/[a-z]/',
+                        'message' => 'Your new password must contain at least one lowercase letter',
+                    ]),
+                    new Regex([
+                        'pattern' => '/\d/',
+                        'message' => 'Your new password must contain at least one number',
+                    ]),
+                    new Regex([
+                        'pattern' => '/[!@#$%^&*(),.?":{}|<>]/',
+                        'message' => 'Your new password must contain at least one special character (!@#$%^&*(),.?":{}|<>)',
+                    ]),
+                ],
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Change password',
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,24 +4,51 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Regex;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Email;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('username')
-            ->add('email', EmailType::class)
+            ->add('username', null, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a username',
+                    ]),
+                    new Length([
+                        'min' => 4,
+                        'max' => 20,
+                        'minMessage' => 'Your username must be at least {{ limit }} characters long',
+                        'maxMessage' => 'Your username cannot be longer than {{ limit }} characters',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9]+$/',
+                        'message' => 'Your username can only contain letters and numbers',
+                    ]),
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'constraints' => [
+                    new Email([
+                        'message' => 'Please enter a valid email address.',
+                    ]),
+                    new NotBlank([
+                        'message' => 'Please enter an email address.',
+                    ]),
+                ]
+            ])
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'first_options' => ['label' => 'Password'],
@@ -33,7 +60,9 @@ class RegistrationFormType extends AbstractType
                     ]),
                     new Length([
                         'min' => 12,
-                        'minMessage' => 'Your password must be at least 12 characters long',
+                        'max' => 64,
+                        'minMessage' => 'Your password must be at least {{ limit }} characters long',
+                        'maxMessage' => 'Your password must not be longer than {{ limit }} characters',
                     ]),
                     new Regex([
                         'pattern' => '/[A-Z]/',
@@ -48,8 +77,8 @@ class RegistrationFormType extends AbstractType
                         'message' => 'Your password must contain at least one number',
                     ]),
                     new Regex([
-                        'pattern' => '/[@$!%*#?&]/',
-                        'message' => 'Your password must contain at least one special character (@$!%*#?&)',
+                        'pattern' => '/[!@#$%^&*(),.?":{}|<>]/',
+                        'message' => 'Your password must contain at least one special character (!@#$%^&*(),.?":{}|<>)',
                     ]),
                 ]
             ])
@@ -59,14 +88,8 @@ class RegistrationFormType extends AbstractType
                     'Customer' => 'customer',
                 ],
                 'label' => 'Role',
-            ])
-            ->add('addresses', CollectionType::class, [
-                'entry_type' => AdressFormType::class,
-                'entry_options' => ['label' => false],
-                'allow_add' => true,
-                'allow_delete' => true,
-                'by_reference' => false,
-                'mapped' => true,
+                'multiple' => false,
+                'expanded' => false,
             ]);
     }
 
