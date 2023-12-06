a1dc7d3b6b073233dd47e7242b7fd0a88d4a4777
Kazae67 <Kazae.zaeka@gmail.com>
2023-12-06 14:35:48 +0100

    ajout filtre dynamique js

---
+++ public/js/select-filter.js
@@ -0,0 +1,6 @@
+document.getElementById('category-select').addEventListener('change', function() {
+    document.getElementById('filter-form').submit();
+});
+document.getElementById('sort-select').addEventListener('change', function() {
+    document.getElementById('filter-form').submit();
+});
