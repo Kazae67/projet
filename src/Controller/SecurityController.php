73621f3e976916d13635b2a08d9af9323dae87df
Yasin AKGEDIK <yasin.akgedik@stagiaire-elan.fr>
2023-12-08 17:04:00 +0100

    problème d'anonymisation terminé

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,10 +7,12 @@ use App\Entity\Order;
 use App\Entity\Review;
 use App\Repository\ProductRepository;
 use App\Repository\OrderRepository;
+use App\Repository\OrderDetailRepository;
 use App\Repository\ReviewRepository;
 use App\Repository\AdressRepository;
 use App\Form\PasswordConfirmationFormType;
 use App\DTO\PasswordConfirmationModel;
+use App\Entity\OrderDetail;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -52,56 +54,7 @@ class SecurityController extends AbstractController
         // La logique de déconnexion sera gérée automatiquement par Symfony donc je laisse vide
     }
 
-    #[Route('/user/anonymize', name: 'user_anonymize', methods: ['POST'])]
-    public function anonymizeUser(Request $request, EntityManagerInterface $em): Response
-    {
-        $user = $this->getUser();
-        if (!$user) {
-            throw $this->createAccessDeniedException('You must be logged in to anonymize your account.');
-        }
 
-        if ($this->isCsrfTokenValid('anonymize_user', $request->request->get('_token'))) {
-            // Anonymiser les adresses liées à l'utilisateur
-            $addresses = $em->getRepository(Adress::class)->findBy(['user' => $user]);
-            foreach ($addresses as $address) {
-                $address->setStreet('Anonymized');
-                $address->setCity('Anonymized');
-                $address->setState('Anonymized');
-                $address->setPostalCode('00000');
-                $address->setCountry('Anonymized');
-                $em->persist($address);
-            }
-
-            // Anonymiser les commandes liées à l'utilisateur
-            $orders = $em->getRepository(Order::class)->findBy(['user' => $user]);
-            foreach ($orders as $order) {
-                $order->setFirstName('Anonymized');
-                $order->setLastName('Anonymized');
-                $em->persist($order);
-            }
-
-            // Anonymiser les avis laissés par l'utilisateur
-            $reviews = $em->getRepository(Review::class)->findBy(['user' => $user]);
-            foreach ($reviews as $review) {
-                $review->setTitle('Anonymized Review');
-                $review->setComment('This review has been anonymized.');
-                $em->persist($review);
-            }
-
-            // Remplacer les données personnelles de l'utilisateur par des valeurs génériques
-            $user->setUsername('DeletedUser' . rand(1000, 9999));
-            $user->setEmail('anonyme' . rand(1000, 9999) . '@example.com');
-
-            // Mettre à jour l'utilisateur dans la base de données
-            $em->persist($user);
-            $em->flush();
-
-            // Déconnecter l'utilisateur après l'anonymisation
-            return $this->redirectToRoute('app_logout');
-        }
-
-        return $this->redirectToRoute('app_profile');
-    }
 
     #[Route('/user/confirm-password', name: 'user_confirm_password', methods: ['POST'])]
     public function confirmPassword(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em, ProductRepository $productRepository, AdressRepository $addressRepository, OrderRepository $orderRepository, ReviewRepository $reviewRepository): JsonResponse
@@ -118,10 +71,16 @@ class SecurityController extends AbstractController
         }
 
         // Supprimer tous les produits associés à l'utilisateur
-        $products = $productRepository->findBy(['user' => $user]);
-        foreach ($products as $product) {
-            $em->remove($product);
-        }
+         $products = $productRepository->findBy(['user' => $user]);
+         foreach ($products as $product) {
+            //  $em->remove($product);
+            $product->setPrice(0);
+            $product->setStockQuantity(0);
+            $product->setName('Deleted'. rand(1000, 9999));
+            $product->setDescription('This product is deleted');
+            $product->setActive(false);
+
+         }
 
         // Anonymiser les adresses liées à l'utilisateur
         $addresses = $addressRepository->findBy(['user' => $user]);
@@ -129,7 +88,7 @@ class SecurityController extends AbstractController
             $address->setStreet('Anonymized');
             $address->setCity('Anonymized');
             $address->setState('Anonymized');
-            $address->setPostalCode('00000');
+            $address->setPostalCode('XX-000');
             $address->setCountry('Anonymized');
             $em->persist($address);
         }
@@ -152,7 +111,8 @@ class SecurityController extends AbstractController
 
         // Remplacer les données personnelles de l'utilisateur par des valeurs génériques
         $user->setUsername('DeletedUser' . rand(1000, 9999));
-        $user->setEmail('anonyme' . rand(1000, 9999) . '@example.com');
+        $user->setEmail('anonyme' . rand(1000, 9999) . '@xxx.x');
+        $user->setIsActivated(0);
 
         // Mettre à jour l'utilisateur dans la base de données
         $em->persist($user);
