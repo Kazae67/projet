eeac6cbd6987b682643e6c113e5b3b56c865aef3
Kazae67 <Kazae.zaeka@gmail.com>
2023-12-12 06:44:58 +0100

    amélioration du new-adress.js pour cacher à nouveau le formulaire si autre est sélectionné que new adress

--- public/js/new-adress.js
+++ public/js/new-adress.js
@@ -1,18 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const addressRadios = document.querySelectorAll('input[name="order_confirmation_form[selectedAddress]"]');
     const newAddressForm = document.getElementById('new_address_form');
-    const newAddressRadio = document.querySelector('.new-address-radio');
 
     // Fonction pour afficher/masquer le formulaire d'adresse
     const toggleAddressForm = function() {
-        if (newAddressRadio) {
-            newAddressForm.style.display = newAddressRadio.checked ? 'block' : 'none';
-        }
+        // Affiche le formulaire seulement si "new address" est sélectionné
+        const newAddressSelected = document.querySelector('.new-address-radio:checked');
+        newAddressForm.style.display = newAddressSelected ? 'block' : 'none';
     };
 
-    // Ajouter l'événement de changement sur le bouton radio pour la nouvelle adresse
-    if (newAddressRadio) {
-        newAddressRadio.addEventListener('change', toggleAddressForm);
-    }
+    // Ajouter l'événement de changement sur tous les boutons radio
+    addressRadios.forEach(radio => radio.addEventListener('change', toggleAddressForm));
 
     // Appeler la fonction initialement pour configurer l'état correct
     toggleAddressForm();
